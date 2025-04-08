16365a80c5e66b128d006b9d88c0d01b61603cd0
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-08 13:10:45 +0300

    Feat: Modelling our Author model done

--- src/main/java/org/example/gmatieso/bookmgnmentsystem/models/Author.java
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/models/Author.java
@@ -4,15 +4,20 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
-public class Book {
+public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    @ManyToOne
-    private Author author;
-    private String description;
+
+    private String name;
+    private String biography;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+
 }
