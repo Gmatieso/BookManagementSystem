29a9532b55993f3025f80f2bbc039a5d240de860
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-08 10:32:31 +0300

    Fix: Mapping our models done

---
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/models/Book.java
@@ -0,0 +1,20 @@
+package org.example.gmatieso.bookmgnmentsystem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+}
