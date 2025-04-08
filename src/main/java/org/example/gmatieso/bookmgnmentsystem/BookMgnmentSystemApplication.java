8cc7b6c0d0f5fce5d9e0dd140ed4d293e1c2abb2
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-08 10:05:56 +0300

    Refactor: Added new files

---
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/BookMgnmentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.gmatieso.bookmgnmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMgnmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMgnmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gmatieso/bookmgnmentsystem/BookMgnmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gmatieso.bookmgnmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMgnmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
