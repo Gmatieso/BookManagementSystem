8455da1852f8ffadd703b7b41c5a7fa3d4a1e30f
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-12 11:04:58 +0300

    Annotating our repository with @Repository annotations

--- src/main/java/org/example/gmatieso/bookmgnmentsystem/repository/AuthorRepository.java
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/repository/AuthorRepository.java
@@ -2,6 +2,8 @@ package org.example.gmatieso.bookmgnmentsystem.repository;
 
 import org.example.gmatieso.bookmgnmentsystem.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
--- src/main/java/org/example/gmatieso/bookmgnmentsystem/repository/BookRepository.java
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/repository/BookRepository.java
@@ -3,9 +3,11 @@ package org.example.gmatieso.bookmgnmentsystem.repository;
 import org.example.gmatieso.bookmgnmentsystem.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
       List<Book> findByTitle(String title);  // ! SELECT b FROM Book b WHERE b.title = :title
       List<Book> findByTitleContaining(String keyword); // ! SELECT b FROM BOOK b WHERE b.title LIKE %:keyword%.
