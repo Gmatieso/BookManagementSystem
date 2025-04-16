e4bc2e68758c5297822e4abc70bfd3bd06b37f7e
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-16 11:26:57 +0300

    Added /auth/** to AUTH_WHITELIST for login endpoints

---
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/SecurityConfig.java
@@ -0,0 +1,52 @@
+package org.example.gmatieso.bookmgnmentsystem;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/public/**",
+            "/auth/**" // Allow login endpoints
+    };
+
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(req -> req
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .requestMatchers("/books/**", "/authors/**", "/categories/**").authenticated() // Require auth
+                        .anyRequest().authenticated())
+                .logout(logoutConfigurer-> logoutConfigurer
+                        .logoutUrl("/auth/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            SecurityContextHolder.clearContext();
+                              ApiResponse<String> apiResponse = new ApiResponse<>(200,"Logout successfully");
+                            response.setStatus(HttpServletResponse.SC_OK);
+                            response.setContentType("application/json");
+                            response.getWriter().write(new ObjectMapper().writeValueAsString(apiResponse));
+                            response.getWriter().flush();
+                        })
+                )
+                .build();
+    }
+
+}
