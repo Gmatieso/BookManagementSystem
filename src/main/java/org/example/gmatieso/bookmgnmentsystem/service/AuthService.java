058432e70fccd7ea99244d8c90320ee6b641613f
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-16 13:02:37 +0300

    Handles login and token generation.

---
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/service/AuthService.java
@@ -0,0 +1,24 @@
+package org.example.gmatieso.bookmgnmentsystem.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gmatieso.bookmgnmentsystem.utils.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public String login(String username, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return jwtUtil.generateToken(userDetails);
+    }
+}
