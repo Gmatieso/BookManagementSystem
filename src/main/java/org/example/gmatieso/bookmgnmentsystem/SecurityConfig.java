c39725de5b79738dd6ba1d2cdfb570881b0d4a53
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-16 13:20:26 +0300

    Integrate JWT authentication and remove the LogoutHandler dependency.

--- src/main/java/org/example/gmatieso/bookmgnmentsystem/SecurityConfig.java
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/SecurityConfig.java
@@ -7,11 +7,17 @@ import lombok.RequiredArgsConstructor;
 import org.example.gmatieso.bookmgnmentsystem.dtos.ApiResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
@@ -22,29 +28,24 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/public/**",
-            "/auth/**" // Allow login endpoints
+            "/auth/login" // Allow login endpoints
     };
 
-    private final LogoutHandler logoutHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .csrf(csrf-> csrf.disable())
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .requestMatchers("/books/**", "/authors/**", "/categories/**", "book-details/**").authenticated() // Require auth
                         .anyRequest().authenticated())
-
-                .formLogin(form -> form
-                        .loginPage("/auth/login")
-                        .loginProcessingUrl("/auth/login")
-                        .defaultSuccessUrl("/books")
-                        .permitAll())
-
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logoutConfigurer-> logoutConfigurer
                         .logoutUrl("/auth/logout")
-                        .addLogoutHandler(logoutHandler)
                         .logoutSuccessHandler((request, response, authentication) -> {
                             SecurityContextHolder.clearContext();
                               ApiResponse<String> apiResponse = new ApiResponse<>(200,"Logout successfully");
@@ -55,6 +56,16 @@ public class SecurityConfig {
                         })
                 )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
