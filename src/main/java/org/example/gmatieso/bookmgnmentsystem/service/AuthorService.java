1aa46a6bbe6d94dadaef7812f60dfd033cd49107
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-17 11:20:20 +0300

    Feat: Create an AuthorService interface and implementation

---
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/service/AuthorService.java
@@ -0,0 +1,4 @@
+package org.example.gmatieso.bookmgnmentsystem.service;
+
+public interface AuthorService {
+}
---
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/service/AuthorServiceImpl.java
@@ -0,0 +1,4 @@
+package org.example.gmatieso.bookmgnmentsystem.service;
+
+public class AuthorServiceImpl implements AuthorService{
+}
