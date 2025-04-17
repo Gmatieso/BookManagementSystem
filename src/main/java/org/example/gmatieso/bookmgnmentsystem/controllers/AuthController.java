23b0eca0f4f0ee878063c96545be2e8c093e2ed8
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-17 10:38:13 +0300

    Fix: Modelling method for Signup done

--- src/main/java/org/example/gmatieso/bookmgnmentsystem/controllers/AuthController.java
+++ src/main/java/org/example/gmatieso/bookmgnmentsystem/controllers/AuthController.java
@@ -1,9 +1,9 @@
 package org.example.gmatieso.bookmgnmentsystem.controllers;
 
 
-import lombok.RequiredArgsConstructor;
 import org.example.gmatieso.bookmgnmentsystem.dtos.LoginRequest;
 import org.example.gmatieso.bookmgnmentsystem.dtos.LoginResponse;
+import org.example.gmatieso.bookmgnmentsystem.dtos.SignupRequest;
 import org.example.gmatieso.bookmgnmentsystem.service.AuthService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +23,11 @@ public class AuthController {
     public LoginResponse login(@RequestBody LoginRequest request){
         String token = authService.login(request.username(), request.password());
         return new LoginResponse(token);
+    }
 
+    @PostMapping("/signup")
+    public LoginResponse signup(@RequestBody SignupRequest request){
+        String token = authService.signup(request.username(), request.password(), request.role()).getUsername();
+        return new LoginResponse(token);
     }
 }
